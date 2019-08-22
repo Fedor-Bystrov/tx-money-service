7af4363130d0b855a1ab6902195e2167302426f4
fedor-bystrov <augustbystrov@gmail.com>
2019-08-22 19:27:54 +0300

    add embedded h2 initialization

--- src/main/java/com/bank/Application.java
+++ src/main/java/com/bank/Application.java
@@ -2,10 +2,30 @@ package com.bank;
 
 import io.javalin.Javalin;
 import io.javalin.core.JavalinConfig;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
 public class Application {
+    private static final Logger LOGGER = LoggerFactory.getLogger(Application.class);
+    private static final String H2_URL = "jdbc:h2:mem:";
+
     public static void main(String[] args) {
-        final var app = Javalin.create(JavalinConfig::enableDevLogging).start(7000);
-        app.get("/", ctx -> ctx.result("Hello World"));
+//        final var app = Javalin.create(JavalinConfig::enableDevLogging).start(7000);
+//        app.get("/", ctx -> ctx.result("Hello World"));
+        LOGGER.info("Initializing embedded database");
+        try (final var connection = DriverManager.getConnection(H2_URL);
+             final var stm = connection.createStatement();
+             final var rs = stm.executeQuery("SELECT 1+1")) {
+
+            if (rs.next()) {
+                System.out.println(rs.getInt(1));
+            }
+
+        } catch (SQLException ex) {
+            LOGGER.error("Embedded database initialization failure", ex);
+        }
     }
 }
