020cf70ed390c889c1474448f75b3c5bab4d4896
fedor-bystrov <augustbystrov@gmail.com>
2019-08-22 18:22:22 +0300

    enable logging

--- src/main/java/com/bank/Application.java
+++ src/main/java/com/bank/Application.java
@@ -1,10 +1,11 @@
 package com.bank;
 
 import io.javalin.Javalin;
+import io.javalin.core.JavalinConfig;
 
 public class Application {
     public static void main(String[] args) {
-        Javalin app = Javalin.create().start(7000);
+        final var app = Javalin.create(JavalinConfig::enableDevLogging).start(7000);
         app.get("/", ctx -> ctx.result("Hello World"));
     }
 }
