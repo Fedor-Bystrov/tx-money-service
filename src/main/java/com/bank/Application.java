6c3e413cea233081ca071dbd29e4a2a70356fc99
fedor-bystrov <augustbystrov@gmail.com>
2019-08-22 17:56:19 +0300

    add javalin dependency

---
+++ src/main/java/com/bank/Application.java
@@ -0,0 +1,10 @@
+package com.bank;
+
+import io.javalin.Javalin;
+
+public class Application {
+    public static void main(String[] args) {
+        Javalin app = Javalin.create().start(7000);
+        app.get("/", ctx -> ctx.result("Hello World"));
+    }
+}
