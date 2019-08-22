60a8cf2aac941a1a930098a7bd962f819c0f665e
fedor-bystrov <augustbystrov@gmail.com>
2019-08-22 18:49:00 +0300

    add account entity

---
+++ src/main/java/com/bank/entity/Account.java
@@ -0,0 +1,34 @@
+package com.bank.entity;
+
+import java.util.Objects;
+
+public class Account {
+    private int accountId;
+
+    public Account(int accountId) {
+    }
+
+    public int getAccountId() {
+        return accountId;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Account account = (Account) o;
+        return accountId == account.accountId;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(accountId);
+    }
+
+    @Override
+    public String toString() {
+        return "Account{" +
+                "accountId=" + accountId +
+                '}';
+    }
+}
