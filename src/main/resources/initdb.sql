6372760f692804948586fbcd4d58f1830858f173
fedor-bystrov <augustbystrov@gmail.com>
2019-08-23 13:41:17 +0300

    add initdb.sql

---
+++ src/main/resources/initdb.sql
@@ -0,0 +1,38 @@
+CREATE TABLE accounts
+(
+    account_id INT AUTO_INCREMENT PRIMARY KEY
+);
+
+CREATE TABLE transactions
+(
+    transaction_id INT AUTO_INCREMENT PRIMARY KEY,
+    creation_time  TIMESTAMP      NOT NULL DEFAULT now(),
+    amount         NUMERIC(10, 2) NOT NULL CHECK (amount > 0),
+    recipient      INT REFERENCES accounts (account_id),
+    sender         INT REFERENCES accounts (account_id)
+);
+
+INSERT INTO accounts
+VALUES (1), -- account(1) is admin account
+       (2),
+       (3),
+       (4),
+       (5),
+       (6);
+
+--
+-- Balances after initialization:
+---------------------------------
+-- account 1: 1_000_000.00 coins
+-- account 2: 500_000.00 coins
+-- account 3: 500_000.00 coins
+-- account 4: 474_999.75 coins
+-- account 5: 25_000.25 coins
+-- account 6: 0 coins
+--
+INSERT INTO transactions (amount, recipient, sender)
+VALUES (2500000.00, 1, 1), -- initializing account(1) with 2_500_000.00
+       (500000.00, 2, 1),  -- account(1) sent 500_000.00 to account(2)
+       (500000.00, 3, 1),  -- account(1) sent 500_000.00 to account(3)
+       (500000.00, 4, 1),  -- account(1) sent 500_000.00 to account(4)
+       (25000.25, 5, 4);   -- account(4) sent 25_000.25  to account(5)
