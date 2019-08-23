f60734468838dd999752849b341c1d56965d84ae
fedor-bystrov <augustbystrov@gmail.com>
2019-08-23 11:05:04 +0300

    clean up entities

--- src/main/java/com/bank/entity/Account.java
+++ src/main/java/com/bank/entity/Account.java
@@ -3,32 +3,32 @@ package com.bank.entity;
 import java.util.Objects;
 
 public class Account {
-    private int accountId;
+  private int accountId;
 
-    public Account(int accountId) {
-    }
+  public Account(int accountId) {
+  }
 
-    public int getAccountId() {
-        return accountId;
-    }
+  public int getAccountId() {
+    return accountId;
+  }
 
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Account account = (Account) o;
-        return accountId == account.accountId;
-    }
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) return true;
+    if (o == null || getClass() != o.getClass()) return false;
+    Account account = (Account) o;
+    return accountId == account.accountId;
+  }
 
-    @Override
-    public int hashCode() {
-        return Objects.hash(accountId);
-    }
+  @Override
+  public int hashCode() {
+    return Objects.hash(accountId);
+  }
 
-    @Override
-    public String toString() {
-        return "Account{" +
-                "accountId=" + accountId +
-                '}';
-    }
+  @Override
+  public String toString() {
+    return "Account{" +
+      "accountId=" + accountId +
+      '}';
+  }
 }
--- src/main/java/com/bank/entity/Transaction.java
+++ src/main/java/com/bank/entity/Transaction.java
@@ -5,61 +5,61 @@ import java.time.LocalDateTime;
 import java.util.Objects;
 
 public class Transaction {
-    private long transactionId;
-    private LocalDateTime creationTime;
-    private BigDecimal amount;
-    private Account sender;
-    private Account recipient;
+  private long transactionId;
+  private LocalDateTime creationTime;
+  private BigDecimal amount;
+  private Account sender;
+  private Account recipient;
 
-    public Transaction(long transactionId, BigDecimal amount, Account sender, Account recipient) {
-        this.transactionId = transactionId;
-        this.creationTime = LocalDateTime.now();
-        this.amount = amount;
-        this.sender = sender;
-        this.recipient = recipient;
-    }
+  public Transaction(long transactionId, BigDecimal amount, Account sender, Account recipient) {
+    this.transactionId = transactionId;
+    this.creationTime = LocalDateTime.now();
+    this.amount = amount;
+    this.sender = sender;
+    this.recipient = recipient;
+  }
 
-    public long getTransactionId() {
-        return transactionId;
-    }
+  public long getTransactionId() {
+    return transactionId;
+  }
 
-    public LocalDateTime getCreationTime() {
-        return creationTime;
-    }
+  public LocalDateTime getCreationTime() {
+    return creationTime;
+  }
 
-    public BigDecimal getAmount() {
-        return amount;
-    }
+  public BigDecimal getAmount() {
+    return amount;
+  }
 
-    public Account getSender() {
-        return sender;
-    }
+  public Account getSender() {
+    return sender;
+  }
 
-    public Account getRecipient() {
-        return recipient;
-    }
+  public Account getRecipient() {
+    return recipient;
+  }
 
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Transaction that = (Transaction) o;
-        return transactionId == that.transactionId;
-    }
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) return true;
+    if (o == null || getClass() != o.getClass()) return false;
+    Transaction that = (Transaction) o;
+    return transactionId == that.transactionId;
+  }
 
-    @Override
-    public int hashCode() {
-        return Objects.hash(transactionId);
-    }
+  @Override
+  public int hashCode() {
+    return Objects.hash(transactionId);
+  }
 
-    @Override
-    public String toString() {
-        return "Transaction{" +
-                "transactionId=" + transactionId +
-                ", creationTime=" + creationTime +
-                ", amount=" + amount +
-                ", sender=" + sender +
-                ", recipient=" + recipient +
-                '}';
-    }
+  @Override
+  public String toString() {
+    return "Transaction{" +
+      "transactionId=" + transactionId +
+      ", creationTime=" + creationTime +
+      ", amount=" + amount +
+      ", sender=" + sender +
+      ", recipient=" + recipient +
+      '}';
+  }
 }
