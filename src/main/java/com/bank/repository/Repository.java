605ba5e9a8dd1c6e5f5df5d963829492cfbe00f5
fedor-bystrov <augustbystrov@gmail.com>
2019-08-25 11:20:07 +0300

    add findTransaction to repository

--- src/main/java/com/bank/repository/Repository.java
+++ src/main/java/com/bank/repository/Repository.java
@@ -1,9 +1,12 @@
 package com.bank.repository;
 
+import com.bank.exception.EntityNotFoundException;
 import com.bank.pojo.AccountListDto;
+import com.bank.pojo.TransactionDto;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.math.RoundingMode;
 import java.sql.Connection;
 import java.sql.SQLException;
 import java.util.ArrayList;
@@ -12,6 +15,9 @@ import java.util.List;
 public class Repository {
   private static final Logger LOGGER = LoggerFactory.getLogger(Repository.class);
   private static final String ALL_ACCOUNTS_QUERY = "SELECT account_id FROM accounts;";
+  private static final String FIND_TX_BY_ID_QUERY = "SELECT " +
+    "transaction_id, creation_time, amount, recipient, sender " +
+    "FROM transactions where id=%d;";
 
   private final Connection connection;
 
@@ -31,6 +37,29 @@ public class Repository {
       }
 
       return accounts;
+    }
+  }
+
+  /**
+   * Select transaction by {@code transactionId} from database.
+   *
+   * @throws EntityNotFoundException if no transaction with specified id
+   */
+  public TransactionDto findTransaction(int transactionId) throws SQLException {
+    LOGGER.info("Looking for transaction with id = {}", transactionId);
+    try (final var statement = connection.createStatement();
+         final var resultSet = statement.executeQuery(String.format(FIND_TX_BY_ID_QUERY, transactionId))) {
+
+      if (resultSet.next()) {
+        return new TransactionDto(resultSet.getInt(1),
+          resultSet.getTimestamp(2).toLocalDateTime(),
+          resultSet.getBigDecimal(3).setScale(2, RoundingMode.HALF_DOWN),
+          resultSet.getInt(4),
+          resultSet.getInt(5)
+        );
+      } else {
+        throw new EntityNotFoundException();
+      }
     }
   }
 }
---
+++ src/test/java/com/bank/repository/RepositoryTest.java
@@ -0,0 +1,13 @@
+package com.bank.repository;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class RepositoryTest {
+
+  @Test
+  void findTransaction() {
+    // TODO
+  }
+}
