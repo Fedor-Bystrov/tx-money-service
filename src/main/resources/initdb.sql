e106fa18954c5c91e6511db403dd1d5c2676c876
fedor-bystrov <augustbystrov@gmail.com>
2019-08-25 17:47:52 +0300

    use account 0 as admin in initdb.sql

--- src/main/resources/initdb.sql
+++ src/main/resources/initdb.sql
@@ -13,7 +13,8 @@ CREATE TABLE transactions
 );
 
 INSERT INTO accounts
-VALUES (1), -- account(1) is admin account
+VALUES (0), -- account(0) is admin account
+       (1),
        (2),
        (3),
        (4),
@@ -31,7 +32,7 @@ VALUES (1), -- account(1) is admin account
 -- account 6: 0.00 coins
 --
 INSERT INTO transactions (creation_time, amount, recipient, sender)
-VALUES ('2019-08-13 00:00:00', 2500000.00, 1, 1), -- initializing account(1) with 2_500_000.00
+VALUES ('2019-08-13 00:00:00', 2500000.00, 1, 0), -- initializing account(1) with 2_500_000.00
        ('2019-08-13 01:01:00', 500000.00, 2, 1),  -- account(1) sent 500_000.00 to account(2)
        ('2019-08-13 02:02:00', 500000.00, 3, 1),  -- account(1) sent 500_000.00 to account(3)
        ('2019-08-13 03:03:00', 500000.00, 4, 1),  -- account(1) sent 500_000.00 to account(4)
