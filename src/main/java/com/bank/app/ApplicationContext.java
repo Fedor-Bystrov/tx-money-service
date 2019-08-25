3aa4d2ea1ab03640bc6603b68967727fcf8ca112
fedor-bystrov <augustbystrov@gmail.com>
2019-08-25 15:15:38 +0300

    add TransactionService and TransactionResource to ApplicationContext

--- src/main/java/com/bank/app/ApplicationContext.java
+++ src/main/java/com/bank/app/ApplicationContext.java
@@ -2,7 +2,9 @@ package com.bank.app;
 
 import com.bank.repository.Repository;
 import com.bank.resource.AccountResource;
+import com.bank.resource.TransactionResource;
 import com.bank.service.AccountService;
+import com.bank.service.TransactionService;
 
 import java.sql.Connection;
 
@@ -10,15 +12,23 @@ class ApplicationContext {
 
   private final Repository repository;
   private final AccountService accountService;
+  private final TransactionService transactionService;
   private final AccountResource accountResource;
+  private final TransactionResource transactionResource;
 
   public ApplicationContext(Connection connection) {
     this.repository = new Repository(connection);
     this.accountService = new AccountService(repository);
+    this.transactionService = new TransactionService(repository);
     this.accountResource = new AccountResource(accountService);
+    this.transactionResource = new TransactionResource(transactionService);
   }
 
   public AccountResource getAccountResource() {
     return accountResource;
+  }
+
+  public TransactionResource getTransactionResource() {
+    return transactionResource;
   }
 }
