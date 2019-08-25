870cbcc0ae3e76612b3fd81043a53a7948335e1f
fedor-bystrov <augustbystrov@gmail.com>
2019-08-25 15:33:34 +0300

    impl getTransactionById in TransactionResource

--- src/main/java/com/bank/resource/TransactionResource.java
+++ src/main/java/com/bank/resource/TransactionResource.java
@@ -10,8 +10,15 @@ public class TransactionResource {
     this.transactionService = transactionService;
   }
 
+  /**
+   * Resource method for fetching transaction by id.
+   * <strong>id is mandatory and must be > 0</strong>
+   *
+   * @param context javalin request context
+   */
   public void getTransactionById(Context context) {
-    // TODO
+    int transactionId = context.pathParam("transactionId", Integer.class).check(id -> id > 0).get();
+    context.json(transactionService.getTransactionById(transactionId));
   }
 
   public void createTransaction(Context context) {
