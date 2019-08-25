5a93eb2dae48fb299452b8ce6771b86533be5d3b
fedor-bystrov <augustbystrov@gmail.com>
2019-08-25 19:26:44 +0300

    impl Functions#getAccountBalance

--- src/main/java/com/bank/db/Functions.java
+++ src/main/java/com/bank/db/Functions.java
@@ -1,11 +1,33 @@
 package com.bank.db;
 
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.sql.Connection;
+import java.sql.SQLException;
+
 public class Functions {
+  private static final String SELECT_SENT_AMOUNT = "SELECT IFNULL(sum(amount), 0) " +
+    "FROM transactions WHERE sender = %d";
+  private static final String SELECT_RECEIVED_AMOUNT = "SELECT IFNULL(sum(amount), 0) " +
+    "FROM transactions WHERE recipient = %d";
+
   /**
    * H2 analogous of stored procedure.
    * Returns balance of account given accountId
    */
-  public static String getAccountBalance(int accountId) {
-    return "0.00";
+  public static BigDecimal getAccountBalance(Connection connection, int accountId) throws SQLException {
+    try (final var statement = connection.createStatement()) {
+      // select sent amount
+      final var sentResultSet = statement.executeQuery(String.format(SELECT_SENT_AMOUNT, accountId));
+      final var sent = sentResultSet.next() ? sentResultSet.getBigDecimal(1) : BigDecimal.ZERO;
+      sentResultSet.close();
+
+      // select received amount
+      final var receivedResultSet = statement.executeQuery(String.format(SELECT_RECEIVED_AMOUNT, accountId));
+      final var received = receivedResultSet.next() ? receivedResultSet.getBigDecimal(1) : BigDecimal.ZERO;
+      receivedResultSet.close();
+
+      return received.subtract(sent).setScale(2, RoundingMode.HALF_DOWN);
+    }
   }
 }
