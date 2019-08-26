864f93861655c5ce01e7781af9bd87511dae9c71
fedor-bystrov <augustbystrov@gmail.com>
2019-08-26 21:42:46 +0300

    add createTransaction to TransactionResource

--- src/main/java/com/bank/resource/TransactionResource.java
+++ src/main/java/com/bank/resource/TransactionResource.java
@@ -1,9 +1,23 @@
 package com.bank.resource;
 
+import com.bank.exception.DatabaseException;
+import com.bank.exception.EntityNotFoundException;
+import com.bank.exception.NotEnoughMoneyException;
+import com.bank.pojo.PostTransactionDto;
 import com.bank.service.TransactionService;
+import io.javalin.http.BadRequestResponse;
 import io.javalin.http.Context;
+import io.javalin.http.InternalServerErrorResponse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.math.BigDecimal;
+import java.util.Map;
+import java.util.Objects;
 
 public class TransactionResource {
+  private static final Logger LOGGER = LoggerFactory.getLogger(TransactionResource.class);
+
   private final TransactionService transactionService;
 
   public TransactionResource(TransactionService transactionService) {
@@ -22,6 +36,26 @@ public class TransactionResource {
   }
 
   public void createTransaction(Context context) {
-    // TODO
+    // validate request body
+    final var postTransactionDto = context.bodyValidator(PostTransactionDto.class)
+      .check(Objects::nonNull)
+      .check(dto -> dto.getAmount() != null && dto.getAmount().compareTo(BigDecimal.ZERO) > 0)
+      // six accounts overall
+      .check(dto -> dto.getRecipient() > 0 && dto.getRecipient() < 7)
+      .check(dto -> dto.getSender() > 0 && dto.getSender() < 7)
+      .get();
+
+    try {
+      context.json(Map.of("transactionId", transactionService.createTransaction(postTransactionDto)));
+    } catch (EntityNotFoundException ex) {
+      LOGGER.warn("Entity not found exception for {}", postTransactionDto, ex);
+      throw new BadRequestResponse("Invalid sender id");
+    } catch (NotEnoughMoneyException ex) {
+      LOGGER.warn("Not enough money exception for {}", postTransactionDto, ex);
+      throw new BadRequestResponse("Not enough money");
+    } catch (DatabaseException ex) {
+      LOGGER.error("Database exception during transaction creation", ex);
+      throw new InternalServerErrorResponse();
+    }
   }
 }
